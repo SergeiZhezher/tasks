2a2b7e4129be2c56a75f6b80f8f9c9f708c6939a
Sergei Zhezher <zhezhers@gmail.com>
2019-01-20 19:11:40 +0300

    Task4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,43 @@
 package com.company;
 
-    //  TASK 3 - http://euler.jakumo.org/problems/pg/1.html
 public class Main {
-    //        The prime factors of 13195 are 5, 7, 13 and 29.
-    //        What is the largest prime factor of the number 6848895 ?
+    // TASK 4 - http://euler.jakumo.org/problems/pg/1.html
 
-    //          в оригіналі задачі вказано число 600851475143 - воно не вміщається в жодну змінну
-   public static boolean bool;
-   public static int number = 6848895;
+//    A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//    Find the largest palindrome made from the product of two 3-digit numbers.
+
+    static boolean b = false;
+    static int number = 999;
+    static int palindromes;
+    static int LargestPalindrome = 0;
 
     public static void main(String[] args) {
-        func1(number);
+        boolean bool = true;
+         while (bool) {
+            if(number == 100) {bool = false;}
+            else func1();
+        }
+        System.out.println("the biggest palindrome, obtained by multiplying two three-digit numbers " + LargestPalindrome);
     }
 
-    public static void func1(int bb) { // узнаєм самий більшим дільник
-        int b  = bb;
-        stop: for( int i = b - 1 ; i > 0; i--) {
-            if(((b % i) == 0)) {number = i; func2(i); break stop;}
+    public static void func1() { // перемножуєм всі можливі комбінації трьохзначних чисел, і віддаємо кожне число на перевірку
+        for(int i = 100; i <= 999; i++) {
+            String product = "" + number * i;
+            func2(product);
         }
+        number--;
     }
 
-    public static void func2(int n) { // перевіряєм чи цей дільник є простим числом, якщо да то говорим відповідь в іншом випадку
-        int num = 0;                  // шукаємо другий по розміру дільник і перевіряємо чи він є простим число і т.д... поки не знайдемо найбільший простий дільник
-       stop2: for(int i = n - 1; i > 0; i--) {
-             if((n % i) == 0) {
-                 num++;
-             }
-        if(num > 2){ bool = false; break stop2;}
-        else bool = true;
+    public static void func2(String n) { //перевіряємо чи є число паліндромом
+        String num = "";
+        int length = n.length();
+        for (int i = length - 1; i >= 0; i--) {
+            num += n.charAt(i);
         }
-            if (bool == true) {System.out.print("самый большой простой делитеть числа 6848895 это " + number);}
-            else func1(number);
-        }
+        if (n.equals(num)) { b = true; palindromes = Integer.parseInt(n);
+                            if(LargestPalindrome <= palindromes) { LargestPalindrome = palindromes; } // тут взнається найбільший паліндром
+                             }
+        else b = false;
+    }
     }
 
