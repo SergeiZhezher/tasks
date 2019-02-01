a50b90eda0f87820a7d0aab5bb12ad94f899c70e
Sergei Zhezher <zhezhers@gmail.com>
2019-02-01 23:04:41 +0300

    Task 9

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,51 +1,84 @@
 package com.company;
+// TASK 9. Source (in Russian) - http://euler.jakumo.org/problems.html
 
-public class Main extends Line {
+//        A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+//
+//        a2 + b2 = c2
+//        For example, 32 + 42 = 9 + 16 = 25 = 52.
+//
+//        There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+//        Find the product abc.
 
-    //Task 8, Source - http://euler.jakumo.org/problems/pg/1.html
-    //conditions - Наибольшее произведение четырех последовательных цифр в нижеприведенном 1000-значном числе равно 9 × 9 × 8 × 9 = 5832.
-    //Найдите наибольшее произведение тринадцати последовательных цифр в данном числе.
 
+public class Main {
 
-    static int[] nums = new int[numbers.trim().length()];
-    static int n2 = 0;
-    static boolean b = true;
-
-    public static void main(String[] args) { //тут всі числа записуються в массив
-        for (int i = 0; i <= numbers.trim().length() - 1; i++) {
-            nums[i] = Integer.parseInt(String.valueOf(numbers.trim().charAt(i)));
-        }
-        //
-        while (b) {
-            if (n2 != 1000) {
-                func2();
-            }
-            else {b = false; System.out.println( "Greatest product: " + greatestProduct);}
-        }
+    static int a = 3;
+    static int b = 4;
+    static int c = 0;
+    public static void main(String[] args) {
+        GenerationNumbers();
         }
 
-    static int product = 1;
-    static int n3 = 0;
-    static int n1 = 0; // вказує з якого числа начнеться цикл
-    static int greatestProduct = 0;
-
-    public static void func2() { // тут перемножується послідовність чисел
-
-            s: for (int i = n2; i < numbers.trim().length(); i++) {
-                product *= nums[i];
-                n1++;
-                if(n1 == 30) {break s;} //тут можна вказати скільки послідовних цифр  треба взяти
-
+        public static void GenerationNumbers() { // тут генеруються трійки й відаються на перевірки. Коли буде згенерованно потрібну трійку  - знаходиться її добуток.
+        stop: for (int i = a; i <= 1000; i++) {
+                for (int o = b; o <= 1000; o++) {
+                    c = (int) Math.sqrt(c = i * i + o * o); //тут вищитується C
+                    if(check1(i, o)) {
+                        if(check2(i,o,c)) {
+                            if (check3(i,o,c)) {
+                                if (check4(i,o,c)) {
+                                    if ((i + o + c) == 1000) {
+                                        System.out.println(i * o * c);
+                                        break stop;
+                                    }}
+                            }
+                        }
+                    }
                 }
-
-        if (greatestProduct <= product) { //тут взнається добуток(произведения) самой найбільшой послідовності
-            greatestProduct = product;
-        }
-        product = 1;
-        n1 = 0;
-        n2 = ++n3;
             }
         }
+
+        static boolean check1(int a, int b) { // перша перевірка // A або B в точності повинно ділитись на 3 або 4 (таке правило)
+        boolean b1;
+            if(a % 3 == 0 || a % 4 == 0 || b % 3 == 0 || b % 4 == 0) {
+                b1 = true;
+            } else b1 = false;
+            return b1;
+        }
+        static boolean check2(int a, int b , int c) { // друга перевірка // A або B або C повинно ділитися на 5
+            boolean b1;
+            if(a % 5 == 0 || b % 5 == 0 || c % 5 == 0) {
+                b1 = true;
+            } else b1 = false;
+            return b1;
+        }
+        static boolean check3(int a, int b , int c) { // перевірка №3
+            int n = ((c - a) * (c - b) / 2);
+            return sqrt(n);
+        }
+
+        static boolean check4(int a, int b , int c) { // перевірка №4
+            boolean b1;
+            int x = a % 2 == 0 ? a : b;
+            int y = a % 2 != 0 ? a : b;
+            long n = c - x;
+            long n2 = (c - y) / 2;
+            if(sqrt(n) == true && sqrt(n2) == true) {b1 = true;} else b1 = false;
+            return b1;
+
+        }
+
+
+        static boolean sqrt(long n) { // перевіряє чи є число квадратним // ця функція потрібна для третьой і четвертой перевірки
+
+            double d = Math.sqrt(n);         //    long tst = (long)(Math.sqrt(n) + 0.5); //цей закомічений код також взнає чи є число квадратним
+            String s = Double.toString(d);   //    return (tst * tst == n);
+            return (s.endsWith(".0"));
+        }
+
+    }
+
+
 
 
 
